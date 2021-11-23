0f8c6a4ef7f6da63c1540a70d146e82e51798cb2
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2021-11-23 21:30:01 +0200

    UI Update

--- src/quote/QuoteGenerator.java
+++ src/quote/QuoteGenerator.java
@@ -5,6 +5,7 @@ import arc.flabel.*;
 import arc.util.*;
 import arc.struct.Seq;
 import mindustry.*;
+import mindustry.graphics.Pal;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
@@ -17,19 +18,25 @@ public class QuoteGenerator extends Mod{
     
     public QuoteGenerator(){
         Log.info("Loaded Quote Generator constructor.");
-
         // listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
+            int index = 0;
             messages = Seq.with("become funny", "seriously", "πed");
             // show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("");
-                dialog.cont.label(() -> "").update(l -> l.setText(messages.random())).padBottom(8).row();
-                dialog.cont.button("Reroll", () -> {
-                    messages.shuffle();
-                    Log.info("Rerolled some messages.");
-                }).margin(4);
-                dialog.cont.button("Close", dialog::hide).margin(4).padLeft(4);
+                BaseDialog dialog = new BaseDialog("Quote Gen");
+                dialog.cont.labelWrap(() -> messages.get(index)).update(l -> l.setText(messages.get(index))).size(1000f, 100f).padBottom(8f).row();
+                dialog.cont.image(Core.atlas.find("whiteui")).color(Pal.gray).size(1000f, 3.50f).padTop(96f).row();
+                dialog.cont.label(() -> "").update(l -> 
+                   l.setText(messages.get(index))
+                ).padBottom(8f).row();
+                dialog.cont.table(Styles.none, t -> {
+                   t.button("Reroll", () -> {
+                       messages.shuffle();
+                       Log.info("Rerolled some messages.");
+                   }).size(100f, 50f).margin(4f);
+                ).size(100f, 50f).margin(4f).padLeft(4f).center();
+                dialog.cont.button("Close", dialog::hide).size(100f, 50f).margin(4f).padLeft(4f);
                 
                 dialog.show();
             });
